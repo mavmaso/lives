8c8f402a5b27ac81dff5c70d44ef5fd79a385fc3
Marcos Soares <mavmaso@gmail.com>
2020-08-17 00:32:06 -0300

    broadcast live view

--- lib/lives_web/live/counter.ex
+++ lib/lives_web/live/counter.ex
@@ -1,16 +1,27 @@
 defmodule LivesWeb.Counter do
   use LivesWeb, :live_view
 
+  @topic "live"
+
   def mount(_params, _session, socket) do
+    LivesWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, :val, 0)}
   end
 
   def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+    new_state = update(socket, :val, &(&1 +1))
+    LivesWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+    new_state = update(socket, :val, &(&1 - 1))
+    LivesWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
